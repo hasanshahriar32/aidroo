5ba163f215e54e538ebbfbb98abdc966a9e6598d
aidroo <aidrooteam@gmail.com>
2024-08-07 18:26:14 +0600

    connect database

--- src/config/sequalize.js
+++ src/config/sequalize.js
@@ -1,27 +1,28 @@
 // sequelize.js
 
-// import { Sequelize } from "sequelize";
-
-// const sequelize = new Sequelize(
-//   process.env.DATABASE_NAME,
-//   process.env.DATABASE_USER,
-//   process.env.DATABASE_PASSWORD,
-//   {
-//     dialect: "mysql", // Change this according to your database type
-//     dialectModule: require("mysql2"),
-//     host: process.env.DATABASE_HOST, // Database host
-//     port: 3306, // Database port
-//     define: {
-//       timestamps: true, // Add timestamps (createdAt, updatedAt) to models
-//     },
-//   }
-// );
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "mysql", // Change this according to your database type
-  dialectModule: require("mysql2"),
-  logging: false,
-});
+const sequelize = new Sequelize(
+  process.env.DATABASE_NAME,
+  process.env.DATABASE_USER,
+  process.env.DATABASE_PASSWORD,
+  {
+    dialect: "mysql", // Change this according to your database type
+    dialectModule: require("mysql2"),
+    host: process.env.DATABASE_HOST, // Database host
+    port: 3306, // Database port
+    logging: false,
+    define: {
+      timestamps: true, // Add timestamps (createdAt, updatedAt) to models
+    },
+  }
+);
+// import { Sequelize } from "sequelize";
+
+// const sequelize = new Sequelize(process.env.DATABASE_URL, {
+//   dialect: "mysql", // Change this according to your database type
+//   dialectModule: require("mysql2"),
+//   logging: false,
+// });
 
 export default sequelize;
