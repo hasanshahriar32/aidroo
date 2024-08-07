11ba90c24a540d8357d0479f7f4841ac296d8bba
aidroo <aidrooteam@gmail.com>
2024-08-07 20:24:51 +0600

    netlify

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -3,6 +3,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000",
+  timeout: 20000,
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
